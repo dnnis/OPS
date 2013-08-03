f076965d1d0fe805eacc923633c39790b5a1fa10
Dennis <smhchf@gmail.com>
2013-08-03 16:52:57 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OPS::Application.config.secret_key_base = '27ee5259bf93e98b0e019cca13876d30b6e4a8d7a0f6d2a687e1c2c428141ef811701f14fc58bb362d0d2011aa9c2ebf17393c9e463441b57e1b6ff127a39803'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OPS::Application.config.session_store :cookie_store, key: '_OPS_session'
