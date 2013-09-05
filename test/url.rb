97a654fd58b5f4131fac21011d5047c854f66982
Dennis <smhchf@gmail.com>
2013-09-05 18:29:02 +0800

    URL BACKUP

--- test/url.rb
+++ test/url.rb
@@ -1,4 +1,5 @@
 http://haoqicat.com/happypeter/htmlcss-ji-chu-jiao-cheng/3
 http://flatuicolors.com/
-    http://happycasts.net/
-    http://railscasts-china.com/tags/%E5%85%A5%E9%97%A8/episodes
+http://happycasts.net/
+http://railscasts-china.com/tags/%E5%85%A5%E9%97%A8/episodes
+test
